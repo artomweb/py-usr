0b4e204918310c3dac81e4259451885a1300a8ef
artomweb <41686219+artomweb@users.noreply.github.com>
2018-11-30 14:30:01 +0000

    Update username.py

--- username.py
+++ username.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 import time
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 import re
